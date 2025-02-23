96e19d9df6b93c7ad4169952f4d7634bf6ef6144
Lorenz Perszyk <lorenz.perszyk@gmail.com>
2025-02-23 21:27:14 +0100

    changes to the image optimization

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	output: "export", // Enable static export
 	images: {
-		loader: "custom", // Use a custom loader
-		loaderFile: "./netlify-image-loader.js", // Path to your custom loader file
+		unoptimized: true, // Disable image optimization
 	},
 };
 
